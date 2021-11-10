9e060be2b57771e66b78f0734bc26a79bcc276c0
azizulwd <azizulislamgd@gmail.com>
2021-11-11 04:33:29 +0600

    extra section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 import Banner from '../Banner/Banner';
+import Offer from '../Offer/Offer';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Banner></Banner>
+            <Offer></Offer>
             <Footer></Footer>
         </div>
     );
---
+++ src/Pages/Home/Offer/Offer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Offer.css';
+import offer1 from '../../../images/world-icon.png';
+import offer2 from '../../../images/car-sold-logo.png';
+import offer3 from '../../../images/tag-icon.png';
+import offer4 from '../../../images/compare-logo.png';
+
+const Offer = () => {
+    return (
+        <div className="offers">
+            <div className="container offer_inner">
+                <div className="offer">
+                    <div className="offer_img">
+                        <img src={offer1} alt="" />
+                    </div>
+                    <div className="offer_content">
+                        <h3>World's #1</h3>
+                        <p>Largest Auto portal</p>
+                    </div>
+                </div>
+                <div className="offer">
+                    <div className="offer_img">
+                        <img src={offer2} alt="" />
+                    </div>
+                    <div className="offer_content">
+                        <h3>Car sold</h3>
+                        <p>Every 4 minutes</p>
+                    </div>
+                </div>
+                <div className="offer">
+                    <div className="offer_img">
+                        <img src={offer3} alt="" />
+                    </div>
+                    <div className="offer_content">
+                        <h3>Offers</h3>
+                        <p>Stay updated pay less</p>
+                    </div>
+                </div>
+                <div className="offer">
+                    <div className="offer_img">
+                        <img src={offer4} alt="" />
+                    </div>
+                    <div className="offer_content">
+                        <h3>Compare</h3>
+                        <p>Decode the right car</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Offer;
