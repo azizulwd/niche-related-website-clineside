610f76b1c79c0ba977c4a496ad35d306b83cc781
azizulwd <azizulislamgd@gmail.com>
2021-11-12 01:06:35 +0600

    register handler added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 import loginLogo from '../../../images/logo.png';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const [loginData, setLoginData] = useState({});
 
+    const handleOnChanged = e =>{
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = {...loginData};
+        newLoginData[field] = value;
+        console.log(newLoginData);
+        setLoginData(newLoginData);
+    }
 
     const handleLoginForm = e =>{
         e.preventDefault();
+        if(loginData.password !== loginData.password2){
+            alert('Your password did not match');
+            return
+        }
     }
 
     return (
@@ -21,10 +34,10 @@ const Register = () => {
                             <h1>DriveUnit</h1>
                         </div>
                         <form onSubmit={handleLoginForm}>
-                            <input type="text" placeholder="Name" />
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
-                            <input type="password" placeholder="Re-type Password" />
+                            <input onChange={handleOnChanged} type="text" placeholder="Name" name="name" />
+                            <input onChange={handleOnChanged} type="email" placeholder="Email" name="email" />
+                            <input onChange={handleOnChanged} type="password" placeholder="Password" name="password" />
+                            <input onChange={handleOnChanged} type="password" placeholder="Re-type Password" name="password2" />
                             <button type="submit">Register</button>
                         </form>
                         <Link to="/login">Already have an account? Please Login.</Link>
