c0e8edaff32da99f58eac2954490394b5028b817
azizulwd <azizulislamgd@gmail.com>
2021-11-13 14:56:48 +0600

    explore page done

--- src/Pages/Explore/Explore.js
+++ src/Pages/Explore/Explore.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Home/Product/Product';
+import './Explore.css';
+import explorebg from '../../images/explore-bg.jpg';
 
 const Explore = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/products')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    }, []);
+
+
     return (
-        <div>
-            <h1>Explore</h1>
+        <div className="explore_all_products">
+            <div className="explore_title">
+                <h1>Choose your Dream Car</h1>
+            </div>
+            <div className="container explore_products_inner">
+                {
+                    products.map(product => <Product
+                        key={product._id}
+                        product={product}
+                    ></Product>)
+                }
+            </div>
         </div>
     );
 };
