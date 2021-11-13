831e0abf04b5e41930f8490b460cb03190533c2e
azizulwd <azizulislamgd@gmail.com>
2021-11-13 15:33:46 +0600

    purchase page details done

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import './Purchase.css';
 
 const Purchase = () => {
     let {purchaseId} = useParams();
@@ -18,8 +19,20 @@ const Purchase = () => {
     }, [productsDetails]);
 
     return (
-        <div>
-            <h1>{singleProductDetails?.name}</h1>
+        <div className="pruchase_products">
+            <div className="container purchase_inner">
+                <div className="purchase_product_details">
+                    <img src={singleProductDetails?.img} alt="" />
+                    <div className="purchase_product_info">
+                        <h2>{singleProductDetails?.name}</h2>
+                        <h4>${singleProductDetails?.price} Million</h4>
+                        <p>{singleProductDetails?.description}</p>
+                    </div>
+                </div>
+                <div className="order_form">
+                    <h1>Purchase product</h1>
+                </div>
+            </div>
         </div>
     );
 };
